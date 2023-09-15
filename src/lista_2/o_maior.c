a82dc4fc9764c0ba2316ed37da6e0afa4c7fea0c
Hugo Loiola <hugoloiola.1@gmail.com>
2023-09-15 13:25:55 -0300

    O Maior e numeros pares

--- src/lista_2/o_maior.c
+++ src/lista_2/o_maior.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-  int a, b, c;
+  int a, b, c, maior;
 
   scanf("%d %d %d", &a, &b, &c);
 
-  if (a > b && a > c)
-  {
-    printf("%d eh o maior\n", a);
-  }
-  else if (b > a && b > c)
-  {
-    printf("%d eh o maior\n", b);
-  }
-  else
-  {
-    printf("%d eh o maior\n", c);
-  }
+  maior = (a + b + abs(a - b)) / 2;
+  maior = (maior + c + abs(maior - c)) / 2;
 
+  printf("%d eh o maior\n", maior);
   return 0;
 }
---
+++ src/lista_2/pum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int linhas, numero;
+  int i = 1;
+
+  scanf("%d", &linhas);
+
+  while (i <= 7)
+  {
+    printf("%d %d %d PUM\n", );
+    i++;
+  }
+
+  return 0;
+}
