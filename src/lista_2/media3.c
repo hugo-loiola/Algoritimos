b1786a67c61761431c5d86304af864751ce0b265
Hugo Loiola <hugoloiola.1@gmail.com>
2023-09-16 16:57:45 -0300

    Media 3 feita

--- src/lista_2/media3.c
+++ src/lista_2/media3.c
@@ -3,24 +3,38 @@
 int main()
 {
 
-  float N1, N2, N3, N4, exame, media;
-  scanf("%f %f %f %f", &N1, &N2, &N3, &N4);
+  double N1, N2, N3, N4, exame, media;
+  scanf("%lf %lf %lf %lf", &N1, &N2, &N3, &N4);
 
   media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10.0;
 
-  printf("Media: %.1f\n", media);
+  printf("Media: %.1lf\n", media);
 
   if (media >= 7.0)
   {
-    printf("Aluno aprovado.");
+    printf("Aluno aprovado.\n");
   }
-  else if (media >= 6.9 || media <= 5.0)
+  else if (media <= 6.9 && media >= 5.0)
   {
-    printf("Aluno em exame.");
+    printf("Aluno em exame.\n");
+    scanf("%lf", &exame);
+    printf("Nota do exame: %.1lf\n", exame);
+
+    media = (media + exame) / 2.0;
+    if (media >= 5.0)
+    {
+      printf("Aluno aprovado.\n");
+    }
+    else
+    {
+      printf("Aluno reprovado.\n");
+    }
+
+    printf("Media final: %.1lf\n", media);
   }
   else
   {
-    printf("Aluno reprovado.");
+    printf("Aluno reprovado.\n");
   }
 
   return 0;
