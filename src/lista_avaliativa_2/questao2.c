12333e6b73dbf5bf3e67fe4b653731a7b20eaf9b
Hugo Loiola <hugoloiola.1@gmail.com>
2023-11-04 16:57:02 -0300

    questao 3 feita por hj

--- src/lista_avaliativa_2/questao2.c
+++ src/lista_avaliativa_2/questao2.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct jogador
+{
+  char nome[31];
+  char posicao;
+  int forca;
+} Jogador;
+
+typedef struct time
+{
+  char nome[31];
+  int numGoleiros;
+  int numLaterais;
+  int numZagueiros;
+  int numVolantes;
+  int numMeias;
+  int numAtacantes;
+  double forcaPonderada;
+
+} Time;
 
 int main()
 {
+  Time time1, time2;
+  Jogador jogadores[22];
+
+  // Leitura do nome dos times
+  fgets(time1.nome, 31, stdin);
+  time1.nome[strcspn(time1.nome, "\n")] = '\0';
+
   return 0;
 }
