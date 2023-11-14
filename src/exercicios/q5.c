a67dc89404d573ceec472cec6868714d7986e70f
Hugo Loiola <hugoloiola.1@gmail.com>
2023-11-14 10:50:42 -0300

    q5

---
+++ src/exercicios/q5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  int n;
+  scanf("%d", &n);
+
+  int *vetor = (int *) malloc(n*sizeof(int));
+  for(int i = 0; i < n; i++)
+  {
+    printf()
+  }
+  return 0;
+}
