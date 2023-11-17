2d26e077949a62ac9871e9f0d13361382d2a8e72
Hugo Loiola <hugoloiola.1@gmail.com>
2023-11-17 13:38:10 -0300

    teste

--- src/exemplos/teste.c
+++ src/exemplos/teste.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-#include <locale.h>
-
 int main()
 {
-  setlocale(LC_ALL, "");
-  printf("Olá, Bom Dia!\n");
-
+  FILE *fp;
+  char string[100];
+  int i;
+  fp = fopen("teste.txt", "a");
+  if (!fp)
+  {
+    printf("Erro na abertura do arquivo");
+    exit(1);
+  }
+  printf("Entre com a string a ser gravada no arquivo: ");
+  fgets(string, 100, stdin);
+  for (i = 0; string[i]; i++)
+    putc(string[i], fp);
+  fclose(fp);
   return 0;
 }
